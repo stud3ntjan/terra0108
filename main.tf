83d74991126d8c465fa5081283da8b93ca99ea5f
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-08-01 15:02:16 +0200

    update

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "super_instanz" {
   instance_type = var.instance_type
   ami           = "ami-071878317c449ae48"
   subnet_id     = var.subnet_id
-  key_name      = var.key
+  key_name      = var.key_name
 }
 
 # IP ausgeben
